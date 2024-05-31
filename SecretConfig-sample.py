efa0406b070c5f42067a14bef0174f670b96d3e2
jorgebls <iefaz.jorgebedoya@gmail.com>
2024-05-31 14:29:41 -0500

    eliminar secretConfig

--- SecretConfig-sample.py
+++
@@ -1,7 +0,0 @@
-# Recuerde poner aqui los datos de conexion, guardar como SecretConfig.py y meterlo en la carpeta model.
-
-PGHOST=
-PGDATABASE=
-PGUSER=
-PGPASSWORD=
-PGPORT=
--- src/model/SecretConfig.py
+++ src/model/SecretConfig.py
@@ -2,8 +2,8 @@
 
 # Do not expose your Neon credentials to the browser
 
-PGHOST=
-PGDATABASE=
-PGUSER=
-PGPASSWORD=
-PGPORT=5432
+PGDATABASE = 'nombre_de_tu_bd'
+PGUSER = 'tu_usuario'
+PGPASSWORD = 'tu_contraseña'
+PGHOST = 'localhost'
+PGPORT = '5432'
